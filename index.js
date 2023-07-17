797a2b5233956b35226a9190c288f40fb83ea141
Jonathan <jonathan.fetterolf@gmail.com>
2023-07-16 20:04:24 -0400

    completed, passing all tests

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence) => sentence.split(" ").map(
+    (word) => word.slice(0, 1).toUpperCase() + word.slice(1)
+  )).map((element) => element.join(" "))
 }
+
